9ce1811d390667edacdfa887d3a5786dfb476949
Vinicius <79338898+vinipet@users.noreply.github.com>
2023-11-17 11:53:12 -0300

    finalizei
    finalmente, após inumeras tentativas eu consegui, nao pelo metodo proposto mas pelo meu metodo, muito mais simples por sinal

---
+++ projects/nav-bar/sources/scripts/script.js
@@ -0,0 +1,10 @@
+
+function opa(){
+   var esse = document.getElementById('penis')
+   
+   if(esse.classList.contains("active")){
+      esse.classList.remove('active')
+   }else{
+      esse.classList.add('active')
+   }
+}
