ed1ba398ec965200474b1b083c65cd70f3def2cd
Vinicius <79338898+vinipet@users.noreply.github.com>
2023-11-25 12:22:06 -0300

    desisto

--- projects/tela-login-animada/assets/scripts/script.js
+++ projects/tela-login-animada/assets/scripts/script.js
@@ -1,5 +1,7 @@
 const Btn_senha = document.getElementById('btnSenha')
+const Btn_senha1 = document.getElementById('btnSenha1')
 const senha_input = document.getElementById('senha')
+const senha_input1 = document.getElementById('senha1')
 const logar_btn = document.getElementById('btnLogar')
 const left = document.getElementById('left')
 const right = document.getElementById('right')
@@ -16,6 +18,19 @@ function senha_atv() {
       senha_input.type = "text"
       Btn_senha.classList.add('visivel')
       Btn_senha.value = "visibility"
+   }
+}
+function senha_atv1() {
+   if(Btn_senha1.classList.contains('visivel'))
+   {
+      Btn_senha1.value = "visibility_off"
+      Btn_senha1.classList.remove('visivel')
+      senha_input1.type = "password"
+   } else 
+   {
+      senha_input1.type = "text"
+      Btn_senha1.classList.add('visivel')
+      Btn_senha1.value = "visibility"
    }
 }
 
