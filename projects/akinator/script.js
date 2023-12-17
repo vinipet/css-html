6d79455fd1d2611d9282ea8de2b521878244d21a
Vinicius <79338898+vinipet@users.noreply.github.com>
2023-12-17 20:38:01 -0300

    update
    Aquela polida no codigo

--- projects/akinator/script.js
+++ projects/akinator/script.js
@@ -1,63 +1,46 @@
-
 // numeros para a escolha
 let array = []
-for(c=1;c<102;c++)
+for(c=0;c<100;c++)
 {
-   array[c]=c
+   array[c]=c+1
 }
-
-
-// componentes usados
-
-let maior = document.querySelector('#maior')
-let menor = document.querySelector('#menor')
-let acerto = document.querySelector('#acerto')
-let res = document.querySelector('#res')
-let resModal = document.querySelector('#res-modal')
 let chute
-
-
-
-
+// erros
+let erroValorExcedido = `Seu nùmero so pode ter o valor entre 1 e 100. Por favor escolha um número dentro do escopo`
 // logica de chute
-
 let chutar = ()=>{
-  chute=tentativa = array[Math.floor((array.length - 1) / 2)]
-  res.innerHTML = `<p>Seu numero é ${chute}?</p>`
-  resModal.innerHTML = `${chute}!!!`
+  chute =  array[Math.floor((array.length - 1) / 2)]
+  document.querySelector('#res').innerHTML = `<p>Seu numero é ${chute}?</p>`
+  document.querySelector('#res-modal').innerHTML = `${chute}!!!`
 }
- 
-
-// logica de acerto
+// logica de acerto 
 let modal = ()=>{
    let pipi = document.querySelector('#modal')
    pipi.classList.toggle('ativo')
-
    chutar()
 }
-
-
-
+let modalOpen = document.querySelector('#acerto').addEventListener('click', modal)
+let modalClose = document.querySelector('#close').addEventListener('click', modal)
 //logica de maior
-maior.addEventListener('click', mais)
-function mais()
-{
-   for(let value = array.indexOf(chute); value != 0 ; value--){
+let maior = document.querySelector('#maior').addEventListener('click', ()=>{
+   if(chute==1||chute==100){
+      window.alert(erroValorExcedido)
+   }else{
+   for(let value = array.indexOf(chute +1); value != 0 ; value--){
       array.shift()
    }
    chutar()
-}
-
-
+   }
+})
 //logica de menor
-menor.addEventListener('click', menos)
-function menos()
-{
-   for(let value = array.indexOf(chute); value != 0 ; value--){
+let menor = document.querySelector('#menor').addEventListener('click', ()=>{
+   if(chute==1||chute==100){
+      window.alert(erroValorExcedido)
+   }else{
+   for(let value = array.indexOf(chute+1); value != 0 ; value--){
       array.pop()
    }
    chutar()
-}
-
-
+   }
+})
 chutar()
