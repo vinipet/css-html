d82fac961e308ec6882bc6819a25c14a885a5b40
Vinicius <79338898+vinipet@users.noreply.github.com>
2023-12-17 21:09:28 -0300

    update
    sistema de tentativa add
    sistema de recomeçar add

--- projects/akinator/script.js
+++ projects/akinator/script.js
@@ -1,9 +1,12 @@
 // numeros para a escolha
 let array = []
-for(c=0;c<100;c++)
-{
-   array[c]=c+1
+let reset = ()=>{
+   for(c=0;c<100;c++)
+   {
+      array[c]=c+1
+   }
 }
+let tentativas =1
 let chute
 // erros
 let erroValorExcedido = `Seu nùmero so pode ter o valor entre 1 e 100. Por favor escolha um número dentro do escopo`
@@ -11,36 +14,50 @@ let erroValorExcedido = `Seu nùmero so pode ter o valor entre 1 e 100. Por favo
 let chutar = ()=>{
   chute =  array[Math.floor((array.length - 1) / 2)]
   document.querySelector('#res').innerHTML = `<p>Seu numero é ${chute}?</p>`
-  document.querySelector('#res-modal').innerHTML = `${chute}!!!`
+  document.querySelector('#res-modal').innerHTML = `<h1>${chute}!!!</h1> <p>Acertei em ${tentativas} tentativas.</p>`
 }
 // logica de acerto 
 let modal = ()=>{
-   let pipi = document.querySelector('#modal')
-   pipi.classList.toggle('ativo')
+   let modal_element = document.querySelector('#modal')
+   if(modal_element.classList.contains('ativo')){
+      modal_element.classList.remove('ativo')
+   }else{
+      modal_element.classList.add('ativo')
+      reset()
+
+   }
    chutar()
 }
 let modalOpen = document.querySelector('#acerto').addEventListener('click', modal)
 let modalClose = document.querySelector('#close').addEventListener('click', modal)
 //logica de maior
 let maior = document.querySelector('#maior').addEventListener('click', ()=>{
-   if(chute==1||chute==100){
+   if(array.length == 1){
       window.alert(erroValorExcedido)
    }else{
    for(let value = array.indexOf(chute +1); value != 0 ; value--){
       array.shift()
    }
+   tentativas ++
    chutar()
    }
 })
 //logica de menor
 let menor = document.querySelector('#menor').addEventListener('click', ()=>{
-   if(chute==1||chute==100){
+   if(array.length == 1){
       window.alert(erroValorExcedido)
    }else{
    for(let value = array.indexOf(chute+1); value != 0 ; value--){
       array.pop()
    }
+   tentativas ++
    chutar()
    }
 })
+//akinator frames
+let akinator = ()=>{
+   
+}
+
+reset()
 chutar()
